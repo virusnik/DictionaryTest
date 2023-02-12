cd1d90be4a3e4516c76dcfa1b2a6af26c90dc583
Sergio Veliz <>
2023-02-12 14:12:41 +0300

    init commit

---
+++ DictionaryTest/DictionaryTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DictionaryTest
+//
+//  Created by Sergio Veliz on 12.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
